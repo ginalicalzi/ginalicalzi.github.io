364283f7ce7a1aaac71774d0962ec05a5b688190
ginalicalzi <70173829+ginalicalzi@users.noreply.github.com>
2021-05-11 00:08:21 -0400

    Add files via upload

---
+++ mannythecrab/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var mugen = new MUGEN();
+  mugen.initialize({titleScreenImageURL: "./img/manny-title.png", actionRadius: 100, timeBetweenNPCs: 3200, npcCollisionRadius: 100, actionDuration:600, jumpHeight: 270, makeResponsive: true, npcSpeed: 500, removeNPCAfterInteracted: false, maxSimultaneousNPCs: 100});
+});
